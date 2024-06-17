008c54fd950356ae666e878c8ac0c18e738ab348
ValkarVarg <mikewilliamstfb@gmail.com>
2024-06-17 16:52:06 +0100

    updates with comments on article page

--- src/routes/Article.jsx
+++ src/routes/Article.jsx
@@ -8,20 +8,28 @@ import newsApi from "../api-calls/axios";
 const Article = () => {
   const { article_id } = useParams();
   const [article, setArticle] = useState(null);
+  const [comments, setComments] = useState(null);
 
   useEffect(() => {
     if (article_id) {
-      newsApi.get(`/articles/${article_id}`)
+    newsApi.get(`/articles/${article_id}`)
         .then(({ data }) => {
           setArticle(data.article);
         })
         .catch(error => {
           console.error('Error fetching article:', error);
         });
+    newsApi.get(`/articles/${article_id}/comments`)
+        .then(({ data }) => {
+          setComments(data.comments);
+        })
+        .catch(error => {
+          console.error('Error fetching comments:', error);
+        });
     }
   }, [article_id]);
 
-  if (!article) {
+  if (!article || !comments) {
     return <Typography variant="body1">Loading...</Typography>;
   }
 
@@ -73,8 +81,35 @@ const Article = () => {
       </Grid>
     </Paper>
     <Paper elevation={1} sx={{ p:2, mt:2}}>
-        {article.body}
+    <Typography variant="body1">{article.body}</Typography>
     </Paper>
+    <Typography variant="h4" sx={{textAlign: "left"}}>Comments:</Typography>
+    {comments.map((comment) => (
+            <Paper item key={comment.comment_id} sx={{ p: 2, mt:2}}>
+              <Typography>{comment.body}</Typography>
+              <Typography>{comment.author}</Typography>
+              <Grid container spacing={1} alignItems="center">
+                  <Grid item>
+                    <IconButton>
+                      <ThumbUpIcon />
+                    </IconButton>
+                  </Grid>
+                  <Grid item>
+                    <Typography variant="body2">{comment.upVotes}</Typography>
+                  </Grid>
+                  <Grid item>
+                    <IconButton>
+                      <ThumbDownIcon />
+                    </IconButton>
+                  </Grid>
+                  <Grid item>
+                    <Typography variant="body2">{comment.downVotes}</Typography>
+                  </Grid>
+                  <Grid item sx={{ ml: 'auto' }}>
+                    <Typography variant="body2">Score: {comment.votes}</Typography>
+                  </Grid>
+                  </Grid>
+            </Paper>))}
     </>
   );
 };
