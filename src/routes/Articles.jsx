59f412407d04f9355b872d15b81f55aa1eedbdb7
ValkarVarg <mikewilliamstfb@gmail.com>
2024-06-19 11:18:11 +0100

    adds topic selecter to articles which routes to query pages

--- src/routes/Articles.jsx
+++ src/routes/Articles.jsx
@@ -1,33 +1,45 @@
 import * as React from "react";
-import ArticleCard from "./components/Article-card";
-import { Grid, Container, CircularProgress, Box, Typography } from "@mui/material";
+import { useLocation } from 'react-router-dom';
+import ArticleCard from "./components/ArticleCard";
+import TopicSelector from "./components/TopicSelector";
+import {
+  Container,
+  CircularProgress,
+  Box,
+  Typography,
+} from "@mui/material";
 import newsApi from "../api-calls/axios";
 
 const Articles = () => {
   const [articles, setArticles] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
+  const location = useLocation();
 
   React.useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const topic = searchParams.get('topic');
+    
+    setLoading(true);
     newsApi
-      .get("/articles")
+      .get("/articles", { params: { topic } })
       .then(({ data }) => {
         setArticles(data.articles);
-        setLoading(false); 
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
         setLoading(false);
       });
-  }, []);
+  }, [location.search]);
 
   if (loading) {
     return (
       <Box
         sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
         }}
       >
         <Typography>Please be patient, API loading</Typography>
@@ -38,23 +50,30 @@ const Articles = () => {
 
   return (
     <div>
-      <select>
-        <option>select</option>
-        <option>topic placeholder</option>
-      </select>
-      <Container>
-        <Grid container spacing={2}>
-          {articles.map((article) => (
-            <Grid item key={article.article_id} xs={6} sm={3} md={2} lg={2}>
-              <ArticleCard title={article.title} image={article.article_img_url} article_id={article.article_id} />
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
+      <Box sx={{ paddingTop: { xs: '64px', sm: '56px' } }}>
+        <TopicSelector />
+        <Container>
+          <Box 
+            sx={{
+              display: 'grid',
+              gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
+              gap: 2,
+              marginTop: 2,
+            }}
+          >
+            {articles.map((article) => (
+              <ArticleCard
+                key={article.article_id}
+                title={article.title}
+                image={article.article_img_url}
+                article_id={article.article_id}
+              />
+            ))}
+          </Box>
+        </Container>
+      </Box>
     </div>
   );
 };
 
 export default Articles;
-
-
---
+++ src/routes/components/TopicSelector.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { FormControl, InputLabel, NativeSelect } from '@mui/material';
+import { useNavigate, useLocation } from 'react-router-dom';
+import newsApi from '../../api-calls/axios';
+
+const TopicSelector = () => {
+  const [topics, setTopics] = React.useState([]);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  React.useEffect(() => {
+    newsApi
+      .get("/topics")
+      .then(({ data }) => {
+        setTopics(data.topics);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const searchParams = new URLSearchParams(location.search);
+  const selectedTopic = searchParams.get("topic") || "All";
+
+  const handleChange = (event) => {
+    const selectedTopic = event.target.value;
+    if (selectedTopic === "All") {
+      searchParams.delete("topic");
+    } else {
+      searchParams.set("topic", selectedTopic);
+    }
+    navigate({ search: searchParams.toString() });
+  };
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  return (
+    <FormControl fullWidth margin='normal'>
+      <InputLabel variant="standard" htmlFor="uncontrolled-native">
+        Topic
+      </InputLabel>
+      <NativeSelect
+        value={selectedTopic}
+        inputProps={{
+          name: 'topics',
+          id: 'uncontrolled-native',
+        }}
+        onChange={handleChange}
+      >
+        <option value="All">All</option>
+        {topics.map((topic) => (
+          <option key={topic.slug} value={topic.slug}>
+            {capitalizeFirstLetter(topic.slug)}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
+  );
+};
+
+export default TopicSelector;
